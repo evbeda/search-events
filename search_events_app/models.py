55fed0d387d73ff14bfb48b492a537205518d211
Agustin Ramis <aramis@eventbrite.com>
2020-04-21 17:11:01 -0300

    first create event model

--- search_events_app/models.py
+++ search_events_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Feature(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=20)
+    country = models.ForeignKey(
+        Country,
+        on_delete=models.CASCADE
+    )
+    feature = models.ForeignKey(
+        Feature,
+        on_delete=models.CASCADE
+    )
