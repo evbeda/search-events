84b4dcf9a5346cf8f918efc55ea5145283a5cb87
Agustin Ramis <aramis@eventbrite.com>
2020-04-22 10:52:26 -0300

    change models django to models python

--- search_events_app/tests.py
+++ search_events_app/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .models.country import Country
+from .models.event import Event
+from .models.feature import Feature
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.country = Country("Argentina")
+        self.feature = Feature("Embedded Checkout")
+        self.event = Event("Evento1", self.country.name, self.feature.name)
+
+    def test_feature_model(self):
+        self.assertEqual(self.feature.name, "Embedded Checkout")
+
+    def test_country_model(self):
+        self.assertEqual(self.country.name, "Argentina")
+
+    def test_event_model(self):
+        self.assertEqual(self.event.name, "Evento1")
+        self.assertEqual(self.event.country, self.country.name)
+        self.assertEqual(self.event.feature, self.feature.name)
