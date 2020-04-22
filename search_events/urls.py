d6b2c068d770ec0c46ac20f4501eb67f614d7add
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-22 11:12:14 -0300

    create table with events basic info

--- search_events/urls.py
+++ search_events/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import (
+    path,
+    include
+)
+
+from search_events_app import urls as search_events_app_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(search_events_app_urls)),
 ]
--- search_events_app/views.py
+++ search_events_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+
 
 # Create your views here.
+class EventListView(ListView):
+    template_name = 'event_list.html'
+
+    def get_queryset(self):
+        return None
