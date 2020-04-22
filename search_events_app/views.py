582d74e234dae13c22c3ea69ee0c40c2f5720ac8
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-22 15:23:56 -0300

    add events attributes to table

--- search_events_app/views.py
+++ search_events_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 
+from .services.dummy_api import DummyApi
+
 
 # Create your views here.
 class EventListView(ListView):
     template_name = 'event_list.html'
 
     def get_queryset(self):
-        return None
+        return DummyApi().get()
