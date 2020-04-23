8fbcafaaeb909343633267d1da845fb00e33124c
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-23 17:30:59 -0300

    staticfiles_dirs changed

--- search_events/settings/base.py
+++ search_events/settings/base.py
@@ -16,6 +16,10 @@ from search_events.settings import get_env_variable
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+ALLOWED_HOSTS = [
+    'search-events-evb.herokuapp.com',
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
--- search_events/settings/production.py
+++ search_events/settings/production.py
@@ -3,11 +3,6 @@ from .base import *
 import dj_database_url
 
 
-ALLOWED_HOSTS = [
-    '*',
-]
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
