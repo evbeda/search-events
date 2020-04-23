3457571678c661fd3c67b13b3446b2e50323b282
Martin Monzo Gancia <62262316+mmonzo-eb@users.noreply.github.com>
2020-04-23 18:02:13 -0300

    Fix static (#20)
    * fix: statics
    
    * debug true
    
    * debug false
    
    * allowed hosts *
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed
    
    * staticfiles_dirs changed

--- search_events/settings/base.py
+++ search_events/settings/base.py
@@ -16,6 +16,10 @@ from search_events.settings import get_env_variable
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+ALLOWED_HOSTS = [
+    'search-events-evb.herokuapp.com',
+]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -113,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, "static")
-]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 EL_PAGINATION_PER_PAGE = 5
--- search_events/settings/production.py
+++ search_events/settings/production.py
@@ -3,15 +3,12 @@ from .base import *
 import dj_database_url
 
 
-ALLOWED_HOSTS = [
-    'search-events-evb.herokuapp.com',
-]
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 
-
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 DATABASES = {
     'default': {
@@ -26,16 +23,23 @@ DATABASES = {
 DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(DB_FROM_ENV)
 
+
 # Application definition
 
-INSTALLED_APPS += [  # noqa
-    'whitenoise.runserver_nostatic'
-]
-
-MIDDLEWARE += [  # noqa
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+MIDDLEWARE = [  # noqa
+    'django.middleware.security.SecurityMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
