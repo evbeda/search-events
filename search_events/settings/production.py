8e8eb6a6921de239922aa5cef5b54985aace505d
aramis-eb <62262333+aramis-eb@users.noreply.github.com>
2020-04-23 14:48:54 -0300

    change the settings for heroku (#18)

--- search_events/settings/production.py
+++ search_events/settings/production.py
@@ -7,22 +7,6 @@ ALLOWED_HOSTS = [
     'search-events-evb.herokuapp.com',
 ]
 
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -42,8 +26,6 @@ DATABASES = {
 DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(DB_FROM_ENV)
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
 # Application definition
 
 INSTALLED_APPS += [  # noqa
