98bc443c4f99552fb0fc7a1f3ed96afe81efb915
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-24 15:55:28 -0300

    Create Django country model

--- search_events_app/models/country.py
+++ search_events_app/models/country.py
@@ -1,3 +1,15 @@
-class Country:
-    def __init__(self, name):
-        self.name = name
+from django.db import models
+
+
+# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    alpha_2_code = models.CharField(max_length=2)
+    alpha_3_code = models.CharField(max_length=3)
+    flag = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name_plural = "Countries"
