a10c317469ae6b582f774482151d50365c0036d2
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-27 10:36:38 -0300

    change flag attribute from charfield to urlfield in country model

--- search_events_app/models/country.py
+++ search_events_app/models/country.py
@@ -6,7 +6,7 @@ class Country(models.Model):
     name = models.CharField(max_length=100)
     alpha_2_code = models.CharField(max_length=2)
     alpha_3_code = models.CharField(max_length=3)
-    flag = models.CharField(max_length=200)
+    flag = models.URLField()
 
     def __str__(self):
         return f"{self.name}"
