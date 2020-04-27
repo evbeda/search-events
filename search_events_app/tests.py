a629a4dd9c089dd8aa9c891cdd59ee7f0ad47a2a
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-27 15:58:01 -0300

    refactor test attributes

--- search_events_app/tests.py
+++ search_events_app/tests.py
@@ -28,22 +28,36 @@ class TestCountry(TestCase):
 class TestEvent(TestCase):
 
     def setUp(self):
-        self.country = Country.objects.create(name="Argentina", alpha2Code="AR",
-        alpha3Code="ARG", flag="https://restcountries.eu/data/arg.svg")
-        self.event2 = Event("Name", "www.google.com")
         self.feature = Feature("Embedded Checkout")
-        self.event = Event(
+
+    def test_event_without_country(self):
+        event = Event("Name", "www.google.com")
+        self.assertEqual(event.country, None)
+
+    
+    def test_event_basic_info(self):
+        country = Country.objects.create(name="Argentina", alpha2Code="AR",
+        alpha3Code="ARG", flag="https://restcountries.eu/data/arg.svg")
+        event = Event(
             "Evento1", "www.google" +
-            ".com", self.country, self.feature
+            ".com", country, self.feature
         )
-        self.feature2 = Feature("Reserved Seating")        
-        self.new_event = Event(
+        self.assertEqual(event.name, "Evento1")
+        self.assertEqual(event.country, country)
+        self.assertEqual(event.feature, self.feature)
+        self.assertEqual(event.url, "www.google.com")
+
+    def test_event_with_two_features(self):
+        feature = Feature("Reserved Seating")        
+        event = Event(
             "Futbol",
             "www.google.com",
-            feature=[self.feature, self.feature2]
+            feature=[self.feature, feature]
         )
+        self.assertEqual(len(event.feature), 2)
 
-        self.dict_event = {
+    def test_events_more_items(self):
+        dict_event = {
             'name': 'Carats world tour',
             'organizer_name': 'Seung',
             'country': 'Italy',
@@ -57,30 +71,17 @@ class TestEvent(TestCase):
             'category': 'Music',
             'format': 'Festival'
         }
-
-    def test_event_basic_info(self):
-        self.assertEqual(self.event2.country, None)
-        self.assertEqual(self.event.name, "Evento1")
-        self.assertEqual(self.event.country, self.country)
-        self.assertEqual(self.event.feature, self.feature)
-        self.assertEqual(self.event2.url, "www.google.com")
-
-    def test_event_with_two_features(self):
-        self.assertEqual(len(self.new_event.feature), 2)
-
-    def test_events_more_items(self):
-        event = Event(**self.dict_event)
+        event = Event(**dict_event)
         self.assertEqual(event.language, 'English')
         self.assertEqual(event.category, 'Music')
         self.assertEqual(event.format, 'Festival')    
 
 
 class TestFeature(TestCase):
-    def setUp(self):
-        self.feature = Feature("Embedded Checkout")
 
     def test_feature_model(self):
-        self.assertEqual(self.feature.name, "Embedded Checkout")
+        feature = Feature("Embedded Checkout")
+        self.assertEqual(feature.name, "Embedded Checkout")
 
 
 class TestApiService(TestCase):
