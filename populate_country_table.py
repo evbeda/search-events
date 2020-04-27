8dda3f3dd346210cf63cd4c94315b8666c6fac52
Matias Sotelo <msotelo@eventbrite.com>
2020-04-27 10:59:51 -0300

    Adding script to populate country table

---
+++ populate_country_table.py
@@ -0,0 +1,22 @@
+import os
+import django
+import requests
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'search_events.settings.dev')
+django.setup()
+
+from search_events_app.models.country import Country
+
+
+response = requests.get("https://restcountries.eu/rest/v2/all")
+countries_list = response.json()
+
+for country in countries_list:
+	Country(**country).save()
+
+
+
+
+
+
+
+
