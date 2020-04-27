494088440649e762ec77c3993199d2e2c8d27f3a
aramis-eb <62262333+aramis-eb@users.noreply.github.com>
2020-04-27 16:39:33 -0300

    Country: back-end (#29)

--- search_events_app/views.py
+++ search_events_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 
 from search_events_app.services.api_service import get_events
+from search_events_app.models.country import Country
 
 
 # Create your views here.
@@ -10,3 +11,12 @@ class EventListView(ListView):
 
     def get_queryset(self):
         return get_events()
+
+    def get_context_data(self, **kwargs):
+        countries = Country.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['countries'] = countries
+        context['countries_names'] = [
+            {'id': country.id, 'name': country.name} for country in countries
+        ]
+        return context
