f17d6606babdea6ef6fa3d6697d0bf6bf1adc563
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-04-27 10:54:52 -0300

    refactor tests in separated testClasses

--- search_events_app/tests.py
+++ search_events_app/tests.py
@@ -9,50 +9,56 @@ from search_events_app.models.event import Event
 from search_events_app.models.feature import Feature
 
 
-class TestModels(TestCase):
+class TestCountry(TestCase):
+
     def setUp(self):
         self.country = Country.objects.create(name="Argentina", alpha_2_code="AR",
         alpha_3_code="ARG", flag="https://restcountries.eu/data/arg.svg")
-        self.feature = Feature("Embedded Checkout")
-        self.feature2 = Feature("Reserved Seating")
-        self.event = Event(
-            "Evento1", "www.google" +
-            ".com", self.country, self.feature
-            )
-        self.event2 = Event("Name", "www.google.com")
-        self.new_event = Event(
-            "Futbol",
-            "www.google.com",
-            feature=[self.feature, self.feature2]
-            )
-        self.dict_event = {
-                'name': 'Carats world tour',
-                'organizer_name': 'Seung',
-                'country': 'Italy',
-                'url': (
-                    'https://www.eventbrite.com/e/carats-world-tour-tickets'
-                    '-102537931714?aff=ebdssbonlinesearch'
-                    ),
-                'start_date': '2020-07-20',
-                'features': ['EB Studio', 'Facebook'],
-                'language': 'English',
-                'category': 'Music',
-                'format': 'Festival'
-            }
 
-    def test_feature_model(self):
-        self.assertEqual(self.feature.name, "Embedded Checkout")
-
-    def test_country_model(self):
+    def test_country_basic_info(self):
         self.assertEqual(self.country.name, "Argentina")
         self.assertEqual(self.country.alpha_2_code, "AR")
         self.assertEqual(self.country.alpha_3_code, "ARG")
         self.assertEqual(self.country.flag, "https://restcountries.eu/data/arg.svg")
 
-    def test_country_model_str(self):
+    def test_country_str(self):
         self.assertEqual(self.country.__str__(), "Argentina")
 
-    def test_event_model(self):
+    
+class TestEvent(TestCase):
+
+    def setUp(self):
+        self.country = Country.objects.create(name="Argentina", alpha_2_code="AR",
+        alpha_3_code="ARG", flag="https://restcountries.eu/data/arg.svg")
+        self.event2 = Event("Name", "www.google.com")
+        self.feature = Feature("Embedded Checkout")
+        self.event = Event(
+            "Evento1", "www.google" +
+            ".com", self.country, self.feature
+        )
+        self.feature2 = Feature("Reserved Seating")        
+        self.new_event = Event(
+            "Futbol",
+            "www.google.com",
+            feature=[self.feature, self.feature2]
+        )
+
+        self.dict_event = {
+            'name': 'Carats world tour',
+            'organizer_name': 'Seung',
+            'country': 'Italy',
+            'url': (
+                'https://www.eventbrite.com/e/carats-world-tour-tickets'
+                '-102537931714?aff=ebdssbonlinesearch'
+                ),
+            'start_date': '2020-07-20',
+            'features': ['EB Studio', 'Facebook'],
+            'language': 'English',
+            'category': 'Music',
+            'format': 'Festival'
+        }
+
+    def test_event_basic_info(self):
         self.assertEqual(self.event2.country, None)
         self.assertEqual(self.event.name, "Evento1")
         self.assertEqual(self.event.country, self.country)
@@ -63,10 +69,18 @@ class TestModels(TestCase):
         self.assertEqual(len(self.new_event.feature), 2)
 
     def test_events_more_items(self):
-        event_more_items = Event(**self.dict_event)
-        self.assertEqual(event_more_items.language, 'English')
-        self.assertEqual(event_more_items.category, 'Music')
-        self.assertEqual(event_more_items.format, 'Festival')
+        event = Event(**self.dict_event)
+        self.assertEqual(event.language, 'English')
+        self.assertEqual(event.category, 'Music')
+        self.assertEqual(event.format, 'Festival')    
+
+
+class TestFeature(TestCase):
+    def setUp(self):
+        self.feature = Feature("Embedded Checkout")
+
+    def test_feature_model(self):
+        self.assertEqual(self.feature.name, "Embedded Checkout")
 
 
 class TestApiService(TestCase):
