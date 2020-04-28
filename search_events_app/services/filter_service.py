17052a55bf24ba80895f8bb8c93ca50e6edfe039
Agustin Ramis <aramis@eventbrite.com>
2020-04-28 13:05:13 -0300

    Filter country service

---
+++ search_events_app/services/filter_service.py
@@ -0,0 +1,13 @@
+def filter_events(request, list_events):
+    list_events = apply_country_filter(request, list_events)
+    return list_events
+
+
+def apply_country_filter(request, list_events):
+    qs_country = request.GET.get('myCountry')
+    if (qs_country):
+        list_events = [
+            event for event in list_events if event.country
+            in qs_country and event.country != ''
+        ]
+    return list_events
