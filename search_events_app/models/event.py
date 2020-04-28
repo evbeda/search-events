7f0660da2ed16253888aba2a3d88f1d23a6f8675
msotelo-eb <62262326+msotelo-eb@users.noreply.github.com>
2020-04-28 17:51:38 -0300

    Initial integration API search (#37)

--- search_events_app/models/event.py
+++ search_events_app/models/event.py
@@ -1,12 +1,28 @@
 class Event:
 
-    def __init__(self, name, url, country=None, feature=None, **kwargs):
+    def __init__(
+            self,
+            name,
+            url,
+            country=None,
+            feature=None,
+            start_date=None,
+            language=None,
+            category=None,
+            format=None,
+            organizer=None,
+            **kwargs
+    ):
         self.name = name
         self.country = country
         self.url = url
+        self.start_date = start_date
+        self.language = language
+        self.category = category
+        self.format = format
+        self.organizer = organizer
         if not feature:
             self.feature = []
         else:
             self.feature = feature
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+
--- search_events_app/services/api_service.py
+++ search_events_app/services/api_service.py
@@ -1,7 +1,22 @@
+import requests
+import json
+
 from search_events_app.models.event import Event
 from search_events_app.services.dummy_api import DummyApi
 
 
-def get_events():
-	api_events = DummyApi().get()
+def get_events(countries=None):
+	body_dict = format_body()
+	response = requests.post("https://www.evbqaapi.com/v3/destination/search/?token=QF44722VLJLXURKY43HZ", json=body_dict)
+	api_events = response.json()['events']['results']
 	return [Event(**api_event) for api_event in api_events]
+
+
+def format_body():
+	base_dict = {
+		"event_search": {
+			"sort": "default",
+			"page_size": 100
+		}
+	}
+	return base_dict
