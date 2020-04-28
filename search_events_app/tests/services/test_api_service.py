136ad7ee9de9b67f565e2dd5d00cdd1457c4e00d
Matias Sotelo <msotelo@eventbrite.com>
2020-04-28 17:45:15 -0300

    Initial integration API search

--- search_events_app/tests/services/test_api_service.py
+++ search_events_app/tests/services/test_api_service.py
@@ -1,4 +1,7 @@
-from unittest.mock import patch
+from unittest.mock import (
+    patch,
+    MagicMock,
+)
 
 from django.test import TestCase
 
@@ -10,37 +13,42 @@ from search_events_app.services.dummy_api import DummyApi
 class TestApiService(TestCase):
 
     def setUp(self):
-        self.mock_api_response = [
-            {
-                'name': 'Carats world tour',
-                'organizer_name': 'Seung',
-                'country': 'Italy',
-                'url': 'https://www.eventbrite.com/e/carats-world-tour-tickets-102537931714?aff=ebdssbonlinesearch',
-                'start_date': '2020-07-20',
-                'features': ['EB Studio'],
-                'language': 'English',
-                'category': 'Music',
-                'format': 'Festival'
-            },
-            {
-                'name': 'Virtual stitch & bitch',
-                'organizer_name': 'Fashion Revolution',
-                'country': '',
-                'url': 'https://www.eventbrite.co.uk/e/fashion-question-time-tickets-90925824589?aff=ebdssbonlinesearch',
-                'start_date': '2021-01-30',
-                'features': ['Repeating event'],
-                'language': 'English',
-                'category': 'Fashion',
-                'format': 'Expo',
-            },
-        ]
+        self.mock_api_response = {
+            'events': {
+                'results': [
+                    {
+                        'name': 'Carats world tour',
+                        'organizer_name': 'Seung',
+                        'country': 'Italy',
+                        'url': 'https://www.eventbrite.com/e/carats-world-tour-tickets-102537931714?aff=ebdssbonlinesearch',
+                        'start_date': '2020-07-20',
+                        'features': ['EB Studio'],
+                        'language': 'English',
+                        'category': 'Music',
+                        'format': 'Festival'
+                    },
+                    {
+                        'name': 'Virtual stitch & bitch',
+                        'organizer_name': 'Fashion Revolution',
+                        'country': '',
+                        'url': 'https://www.eventbrite.co.uk/e/fashion-question-time-tickets-90925824589?aff=ebdssbonlinesearch',
+                        'start_date': '2021-01-30',
+                        'features': ['Repeating event'],
+                        'language': 'English',
+                        'category': 'Fashion',
+                        'format': 'Expo',
+                    },
+                ]
+            }
+        }
 
-    def test_get_events(self):
-        with patch.object(
-            DummyApi,
-            'get',
-            return_value=self.mock_api_response
-        ):
-            result = api_service.get_events()
-            self.assertIsInstance(result[0], Event)
-            self.assertEqual(len(result), 2)
+    @patch(
+        'search_events_app.services.api_service.requests.post'
+    )
+    def test_get_events(self, mock_post):
+        response = MagicMock()
+        response.json = MagicMock(return_value=self.mock_api_response)
+        mock_post.return_value = response
+        result = api_service.get_events()
+        self.assertIsInstance(result[0], Event)
+        self.assertEqual(len(result), 2)
