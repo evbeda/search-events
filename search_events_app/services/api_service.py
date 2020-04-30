89902bb17079f266004dd906c6b10384afec207e
Agustin Ramis <aramis@eventbrite.com>
2020-04-30 09:49:13 -0300

    Fix lower case country

--- search_events_app/services/api_service.py
+++ search_events_app/services/api_service.py
@@ -8,35 +8,35 @@ from .api_response_processor import process_events
 
 class ApiService:
 
-	events = None
+    events = None
 
-	@classmethod
-	def get_events(cls, country=None):
-		if not cls.events:
-			body_dict = cls.format_body(country)
-			response = requests.post(
-				"https://www.evbqaapi.com/v3/destination/search/?" +
-				"expand.destination_event=primary_organizer" +
-				"%2Cprimary_venue" +
-				"&token=QF44722VLJLXURKY43HZ",
-				json=body_dict
-			)
-			api_events = process_events(response.json())
-			cls.events = [Event(**api_event) for api_event in api_events]
-		return cls.events
+    @classmethod
+    def get_events(cls, country=None):
+        if not cls.events:
+            body_dict = cls.format_body(country)
+            response = requests.post(
+                "https://www.evbqaapi.com/v3/destination/search/?" +
+                "expand.destination_event=primary_organizer" +
+                "%2Cprimary_venue" +
+                "&token=QF44722VLJLXURKY43HZ",
+                json=body_dict
+            )
+            api_events = process_events(response.json())
+            cls.events = [Event(**api_event) for api_event in api_events]
+        return cls.events
 
-	@classmethod
-	def format_body(cls, country):
-		base_dict = {
-			"event_search": {
-				"sort": "default",
-				"page_size": 20,
-			}
-		}
-		if country:
-			base_dict['event_search']['countries']=[country.lower()]
-		return base_dict
-	
-	@classmethod
-	def clear_events(cls):
-		cls.events = None
+    @classmethod
+    def format_body(cls, country):
+        base_dict = {
+            "event_search": {
+                "sort": "default",
+                "page_size": 20,
+            }
+        }
+        if country:
+            base_dict['event_search']['countries'] = [country.lower()]
+        return base_dict
+
+    @classmethod
+    def clear_events(cls):
+        cls.events = None
