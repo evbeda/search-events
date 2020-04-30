65df4dc0e46287a81a5019cdda3a4b6082deeba9
aramis-eb <62262333+aramis-eb@users.noreply.github.com>
2020-04-30 10:54:34 -0300

    Change country alpha2Code to country name (#44)

--- search_events_app/tests/services/test_api_service.py
+++ search_events_app/tests/services/test_api_service.py
@@ -5,6 +5,7 @@ from unittest.mock import (
 
 from django.test import TestCase
 
+from search_events_app.models.country import Country
 from search_events_app.models.event import Event
 from search_events_app.services.api_service import ApiService
 from search_events_app.services.dummy_api import DummyApi
@@ -72,6 +73,18 @@ class TestApiService(TestCase):
         'search_events_app.services.api_service.requests.post'
     )
     def test_get_events(self, mock_post):
+        country1 = Country.objects.create(
+            name='England',
+            alpha2Code='GB',
+            alpha3Code='GBT',
+            flag='https://www.google.com.ar'
+        )
+        country2 = Country.objects.create(
+            name='United States',
+            alpha2Code='US',
+            alpha3Code='USA',
+            flag='https://www.google.com.ar'
+        )
         response = MagicMock()
         response.json = MagicMock(return_value=self.mock_api_response)
         mock_post.return_value = response
