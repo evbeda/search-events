c519dfa314303f1cb8294b22473b39d8a0680c9e
Matias Sotelo <msotelo@eventbrite.com>
2020-05-04 11:17:18 -0300

    Api service refactor: change filter logic

--- search_events_app/services/api_service.py
+++ search_events_app/services/api_service.py
@@ -8,38 +8,32 @@ from .api_response_processor import process_events
 
 class ApiService:
 
-    events = None
+    @classmethod
+    def get_events(cls, dto_filters_array):
+        search_filters = [dto_filter for dto_filter in dto_filters_array if dto_filter.type == "search"]
+        body_dict = cls.format_body(search_filters)
+        response = requests.post(
+            "https://www.evbqaapi.com/v3/destination/search/?" +
+            "expand.destination_event=primary_organizer" +
+            "%2Cprimary_venue" +
+            "&token=QF44722VLJLXURKY43HZ",
+            json=body_dict
+        )
+        api_events = process_events(response.json())
+        return [Event(**api_event) for api_event in api_events]
 
     @classmethod
-    def get_events(cls, country=None):
-		
-        if not cls.events:
-            body_dict = cls.format_body(country)
-            response = requests.post(
-                "https://www.evbqaapi.com/v3/destination/search/?" +
-                "expand.destination_event=primary_organizer" +
-                "%2Cprimary_venue" +
-                "&token=QF44722VLJLXURKY43HZ",
-                json=body_dict
-            )
-            api_events = process_events(response.json())
-            cls.events = [Event(**api_event) for api_event in api_events]
-        return cls.events
-
-    @classmethod
-    def format_body(cls, country):
+    def format_body(cls, dto_filters):
         base_dict = {
             "event_search": {
                 "sort": "default",
-				"dates": "current_future",
+                "dates": "current_future",
                 "page_size": 20,
             }
         }
-        if country:
-            base_dict['event_search']['countries'] = [country.lower()]
-            base_dict['event_search']["online_events_excluded"] = True
+        for dto_filter in dto_filters:
+            base_dict['event_search'].update(dto_filter.value)
+
         return base_dict
 
-    @classmethod
-    def clear_events(cls):
-        cls.events = None
+
