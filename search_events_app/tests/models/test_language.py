5a8a042dfd25d3cd74ae857718860c8703abc5bc
Agustin Ramis <aramis@eventbrite.com>
2020-05-05 17:48:37 -0300

    fix

--- search_events_app/tests/models/test_language.py
+++ search_events_app/tests/models/test_language.py
@@ -3,14 +3,14 @@ from django.test import TestCase
 from search_events_app.models.language import Language
 
 
-class TestCountry(TestCase):
+class TestLanguage(TestCase):
 
     def setUp(self):
-        self.language = Language.objects.create(name="England", code="EN")
+        self.language = Language.objects.create(name="English", code="EN")
 
     def test_language_basic_info(self):
-        self.assertEqual(self.language.name, "England")
+        self.assertEqual(self.language.name, "English")
         self.assertEqual(self.language.code, "EN")
 
     def test_language_str(self):
-        self.assertEqual(self.language.__str__(), "England")
+        self.assertEqual(self.language.__str__(), "English")
