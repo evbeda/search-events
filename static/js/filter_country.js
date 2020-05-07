9364c5247fa260320e0cd43d12718e6720f56353
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-05-07 11:04:41 -0300

    replace url country name in filter

--- static/js/filter_country.js
+++ static/js/filter_country.js
@@ -150,14 +150,13 @@ function toggleDisable(domId, value) {
 }
 
 function querySt(ji) {
-
     hu = window.location.search.substring(1);
     gy = hu.split("&");
 
     for (i=0;i<gy.length;i++) {
         ft = gy[i].split("=");
         if (ft[0] == ji) {
-            return ft[1].replace("+"," ");
+            return ft[1].replace(/\+/g, " ");
         }
     }
 }
