1ab521c5d5d9345b63d69d081d5485311c9db95a
Agustin Ramis <aramis@eventbrite.com>
2020-05-07 10:31:30 -0300

    change production true

--- search_events/settings/production.py
+++ search_events/settings/production.py
@@ -42,4 +42,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
