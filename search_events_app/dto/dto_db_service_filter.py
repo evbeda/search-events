6e55b20da8f65cb33088f6f0bf985f983a265570
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-05-15 12:25:23 -0300

    Create query parameters and test db service

--- search_events_app/dto/dto_db_service_filter.py
+++ search_events_app/dto/dto_db_service_filter.py
@@ -1,5 +1,5 @@
 class DTODBServiceFilter:
 
     def __init__(self, **kwargs):
-        self.join_value = kwargs.get('join_query', '')
-        self.where_value = kwargs.get('where_query', '')
+        self.join_query = kwargs.get('join_query', '')
+        self.where_query = kwargs.get('where_query', '')
