8d48136e86396d647c0cc7c991cdda3c321b80a2
Matias Sotelo <msotelo@eventbrite.com>
2020-05-18 11:47:41 -0300

    Features: Create django model and migrations for DB

--- search_events_app/models/feature.py
+++ search_events_app/models/feature.py
@@ -1,3 +1,13 @@
-class Feature:
-    def __init__(self, name):
-        self.name = name
+from django.db import models
+
+
+class Feature(models.Model):
+
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name_plural = 'Features'
