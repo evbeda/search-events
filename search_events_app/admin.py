01524cda5c7fa74b9d2a11ba67fe171690c922e3
msotelo-eb <62262326+msotelo-eb@users.noreply.github.com>
2020-05-18 15:19:49 -0300

    Features: Create django model and migrations for DB (#102)
    * Features: Create django model and migrations for DB
    
    * Fix Tests

--- search_events_app/admin.py
+++ search_events_app/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 
 from .models.country import Country
+from .models.feature import Feature
 from .models.language import Language
 
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Language)
+admin.site.register(Feature)
