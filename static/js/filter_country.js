28e5fa2120747da2240268e47963aa544f7b1907
aramis-eb <62262333+aramis-eb@users.noreply.github.com>
2020-05-18 14:30:50 -0300

    initial front of feature (#103)

--- static/js/filter_country.js
+++ static/js/filter_country.js
@@ -134,12 +134,21 @@ const filterCriteria = (function() {
 })()
 
 function sendData(domId, array){
+	var selected = [];
+	for (var option of document.getElementById('feature').options) {
+	  if (option.selected) {
+		selected.push(option.value);
+	  }
+	}
+	document.getElementById('textFeature').value = selected.join('-')
 	elem = document.getElementById(domId)
 	data = array.filter(e => e.name == elem.value)
 	if(!data.length && elem.value != "") return false;
 	elem.value = data && data[0] ? data[0].name : ""
 	document.getElementById("country-form").submit()
 }
+
+
 
 function toggleDisable(divId, inputId, value) {
 	div = document.getElementById(divId)
