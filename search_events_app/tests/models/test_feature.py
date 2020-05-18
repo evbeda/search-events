e43eb5f6cef48e67bef8919c07a9cf04665e4271
Martin Monzo Gancia <mmonzo@eventbrite.com>
2020-05-18 17:41:53 -0300

    fix test

--- search_events_app/tests/models/test_feature.py
+++ search_events_app/tests/models/test_feature.py
@@ -6,8 +6,7 @@ from search_events_app.models.feature import Feature
 class TestFeature(TestCase):
 
     def setUp(self):
-        self.feature = Feature.objects.create(name='Embedded Checkout', code='EC')
+        self.feature = Feature.objects.create(name='Website Widgets', code='WW')
 
     def test_feature_model(self):
-        feature = Feature('Website Widgets')
-        self.assertEqual(feature.name, 'Website Widgets')
+        self.assertEqual(self.feature.name, 'Website Widgets')
