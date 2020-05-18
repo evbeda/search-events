f5bddd331ed36e53e83d448c39cf3aabef91327b
Matias Sotelo <msotelo@eventbrite.com>
2020-05-18 12:04:08 -0300

    Fix Tests

--- search_events_app/tests/models/test_feature.py
+++ search_events_app/tests/models/test_feature.py
@@ -5,6 +5,11 @@ from search_events_app.models.feature import Feature
     
 class TestFeature(TestCase):
 
+    def setUp(self):
+        self.feature = Feature.objects.create(name='Embedded Checkout', code='EC')
+
     def test_feature_model(self):
-        feature = Feature('Embedded Checkout')
-        self.assertEqual(feature.name, 'Embedded Checkout')
+        self.assertEqual(self.feature.name, 'Embedded Checkout')
+
+    def test_feature_str(self):
+        self.assertEqual(self.feature.__str__(), 'Embedded Checkout')
