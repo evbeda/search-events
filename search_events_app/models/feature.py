32faae4082afa1b53d5ce1bf12e04fae41d8b514
Matias Sotelo <msotelo@eventbrite.com>
2020-05-18 16:54:00 -0300

    Add get context method to feature model

--- search_events_app/models/feature.py
+++ search_events_app/models/feature.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from search_events_app.mixins.get_context_mixin import GetContextMixin
 
-class Feature(models.Model):
+
+class Feature(models.Model, GetContextMixin):
 
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=2)
@@ -11,3 +13,17 @@ class Feature(models.Model):
 
     class Meta:
         verbose_name_plural = 'Features'
+
+    @classmethod
+    def get_context(cls):
+
+        features = Feature.objects.all()
+
+        return {
+            'features': [
+                {
+                    'code': feature.code,
+                    'name': feature.name,
+                } for feature in features
+            ]
+        }
