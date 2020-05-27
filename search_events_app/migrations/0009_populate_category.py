093242939b818b3c4d61d1108bb84dceb7d4fa86
Agustin Ramis <aramis@eventbrite.com>
2020-05-27 12:28:40 -0300

    fix indentantion

--- search_events_app/migrations/0009_populate_category.py
+++ search_events_app/migrations/0009_populate_category.py
@@ -16,14 +16,14 @@ class Migration(migrations.Migration):
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Family & Education', 'FE');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Fashion & Beauty', 'FB');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Film, Media & Entertainment', 'FME');
-                INSERT INTO main.search_events_app_category (name, code) VALUES ('Food & Drink', 'FD');
+				INSERT INTO main.search_events_app_category (name, code) VALUES ('Food & Drink', 'FD');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Government & Politics', 'GP');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Health & Wellness', 'HW');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Hobbies & Special Interest', 'HSI');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Home & Lifestyle', 'HL');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Music', 'M');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Other', 'O');
-                INSERT INTO main.search_events_app_category (name, code) VALUES ('Performing & Visual Arts', 'PVA');
+				INSERT INTO main.search_events_app_category (name, code) VALUES ('Performing & Visual Arts', 'PVA');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Religion & Spirituality', 'RS');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('School Activities', 'SA');
 				INSERT INTO main.search_events_app_category (name, code) VALUES ('Science & Technology', 'ST');
