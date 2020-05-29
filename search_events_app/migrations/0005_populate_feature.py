739500ac9fb8bd421509d8d61e493478b3acb624
Agustin Ramis <aramis@eventbrite.com>
2020-05-29 11:23:26 -0300

    Implement Donation Filter

--- search_events_app/migrations/0005_populate_feature.py
+++ search_events_app/migrations/0005_populate_feature.py
@@ -9,13 +9,13 @@ class Migration(migrations.Migration):
 	operations = [
 		migrations.RunSQL(
 			'''
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (Add Ons', 'AO');
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (Custom Question', 'CQ');
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (Donation', 'DO');
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (EB Studio', 'ES');
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (Repeating Event', 'RE');
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (Reserved Seating', 'RS');
-				INSERT INTO main.search_events_app_feature (name, code) VALUES (Website Widgets', 'WW');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('Add Ons', 'AO');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('Custom Question', 'CQ');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('Donation', 'DO');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('EB Studio', 'ES');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('Repeating Event', 'RE');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('Reserved Seating', 'RS');
+				INSERT INTO main.search_events_app_feature (name, code) VALUES ('Website Widgets', 'WW');
 			'''
 		)
 	]
