781ae534247eeae4259649f5a042f71e8661eeb0
Agustin Ramis <aramis@eventbrite.com>
2020-06-03 14:54:47 -0300

    City Filter: implement backend and test

---
+++ search_events_app/filters/city_filter.py
@@ -0,0 +1,32 @@
+from django.core.exceptions import ObjectDoesNotExist
+
+from search_events_app.models import City
+from search_events_app.models import Country
+from search_events_app.filters.filter import Filter
+
+
+class CityFilter(Filter):
+
+    def apply_filter(self, request):
+        country_name = request.GET.get('country', '')
+        city_name = request.GET.get('city', '')
+        new_filter = None
+        if city_name:
+            try:
+                country = Country.objects.get(name=country_name)
+                new_filter = City.objects.get(name=city_name, country=country.alpha_2_code )
+            except ObjectDoesNotExist:
+                new_filter = None
+        self.has_changed = new_filter != self.value
+        if self.has_changed:
+            self.value = new_filter
+
+    def get_join_query(self):
+        return ['']
+
+    def get_where_query(self):
+        if self.value:
+            if self.value.code:
+                return f" AND dw_event.event_venue_state = '{self.value.code}' "
+            return f" AND dw_event.event_venue_state = '{self.value.name}' OR dw_event.event_venue_city = '{self.value.name}' "
+        return ''
---
+++ tests/services/filters/test_city_filter.py
@@ -0,0 +1,80 @@
+from unittest.mock import MagicMock
+
+from django.test import TestCase
+
+from search_events_app.models import City
+from search_events_app.filters import CityFilter
+
+
+class TestCityFilter(TestCase):
+
+    def setUp(self):
+        self.city_filter = CityFilter()
+        self.mock_request = MagicMock()
+
+    def test_apply_city_filter(self):
+        request_return = {
+            'country': 'Argentina',
+            'city': 'Mendoza',
+        }
+        self.mock_request.GET = request_return
+
+        self.city_filter.apply_filter(self.mock_request)
+
+        self.assertEqual(self.city_filter.value.name, 'Mendoza')
+        self.assertEqual(self.city_filter.value.country, 'AR')
+        self.assertTrue(self.city_filter.has_changed)
+
+    def test_not_apply_city_filter(self):
+        self.city_filter.apply_filter(self.mock_request)
+
+        self.assertFalse(self.city_filter.has_changed)
+        self.assertIsNone(self.city_filter.value)
+
+    def test_apply_city_filter_with_wrong_data(self):
+        request_return = {
+            'country': 'Argentina',
+            'city': 'Incorrect City',
+        }
+        self.mock_request.GET = request_return
+
+        self.city_filter.apply_filter(self.mock_request)
+
+        self.assertFalse(self.city_filter.has_changed)
+        self.assertIsNone(self.city_filter.value)
+
+    def test_apply_city_filter_same_value(self):
+        request_return = {
+            'country': 'United States',
+            'city': 'Indiana',
+            'code': 'IN',
+        }
+        self.mock_request.GET = request_return
+        city = City.objects.get(name='Indiana', country='US', code='IN')
+        self.city_filter.value = city
+        self.mock_request.GET = request_return
+
+        self.city_filter.apply_filter(self.mock_request)
+
+        self.assertFalse(self.city_filter.has_changed)
+
+    def test_city_filter_info_with_city_US_selected(self):
+
+        self.city_filter.value = City(name='Indiana', country='US', code='IN')
+
+        self.assertEqual(self.city_filter.get_join_query(), [''])
+        self.assertEqual(self.city_filter.get_where_query(), " AND dw_event.event_venue_state = 'IN' ")
+
+    def test_city_filter_info_with_city_selected(self):
+
+        self.city_filter.value = City(name='Mendoza', country='AR')
+
+        self.assertEqual(self.city_filter.get_join_query(), [''])
+        self.assertEqual(self.city_filter.get_where_query(), " AND dw_event.event_venue_state = 'Mendoza' OR dw_event.event_venue_city = 'Mendoza' ")
+
+    def test_city_filter_info_without_city_selected(self):
+
+        self.city_filter.value = None
+
+        self.assertEqual(self.city_filter.get_join_query(), [''])
+        self.assertEqual(self.city_filter.get_where_query(), '')
