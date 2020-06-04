7f7b2483f9cf2c8f93693af0b0b49f9887f91a1e
msotelo-eb <62262326+msotelo-eb@users.noreply.github.com>
2020-06-04 15:11:56 -0300

    Change base settings for production enviornment (#159)

--- search_events/settings/base.py
+++ search_events/settings/base.py
@@ -16,9 +16,7 @@ from search_events.settings import get_env_variable
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-ALLOWED_HOSTS = [
-    'search-events-evb.herokuapp.com',
-]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
